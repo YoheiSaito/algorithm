8302d7d85149920b430719f459416583dda65300
YoheiSaito <teyo.13324@gmail.com>
2018-10-09 14:49:11 +0900

    make fixup

--- RedBlackTree.hpp
+++ RedBlackTree.hpp
@@ -8,9 +8,52 @@ namespace teyo {
 		public BinaryTree<T>
 	{
 		void insertFixup(const Node<T>& ins){
-			while(ins->getRoot()->getColor() == RED){
-				ins->getRoot();
+			while(ins->getRoot()->getColor() == Color::RED){
+				if(ins->getRoot()->getRoot()->getLeft() == ins->getRoot()){
+					auto y = ins->getRoot()->getRoot()->getRight();
+					if(y->getColor() == Color::RED){
+						// case 1 
+						// uncle and parparent is red 
+						// before odd generation will be black (ins is 0)
+						// until uncle is not be red
+						ins->getRoot()->setColor(BLACK);
+						y->setColor(Color::BLACK());
+						y->getRoot()->setColor(Color::RED);
+						ins = y->getRoot();
+					} else if (ins == ins->getRoot()->getRight()){
+						//case 2
+						// self is right side node and uncle is Black
+						ins = ins->getRoot();
+						this->rotateLeft(ins);
+					}
+					ins->getRoot()->setColor(Color::BLACK);
+					ins->getRoot()->getRoot()->setColor(Color::RED);
+					this->rotateRight(ins->getRoot()->getRoot());
+				}else{
+					auto y = ins->getRoot()->getRoot()->getLeft();
+					if(y->getColor() == Color::RED){
+						// case 1 
+						// uncle and parparent is red 
+						// before odd generation will be black (ins is 0)
+						// until uncle is not be red
+						ins->getRoot()->setColor(BLACK);
+						y->setColor(Color::BLACK());
+						y->getRoot()->setColor(Color::RED);
+						ins = y->getRoot();
+					} else if (ins == ins->getRoot()->getLeft()){
+						//case 2
+						// self is right side node and uncle is Black
+						ins = ins->getRoot();
+						this->rotateRight(ins);
+					}
+					//case3
+					//self is left side node.
+					ins->getRoot()->setColor(Color::BLACK);
+					ins->getRoot()->getRoot()->setColor(Color::RED);
+					this->rotateLeft(ins->getRoot()->getRoot());
+				}
 			}
+			this->getRoot()->setColor(Color::BLACK);
 		}
 
 		public:
@@ -30,12 +73,15 @@ namespace teyo {
 		}
 		void insert(T key){
 			auto ins = new Node<T>(key);
-			if(this->root == NULL)
+			if(this->root == NULL){
 				ins->setColor(Color::BLACK);
-			else
+				BinaryTree<T>::insert(ins);
+				insertFixup(ins);
+			}
+			else {
 				ins->setColor(Color::RED);
-			BinaryTree<T>::insert(ins);
-			insertFixup(ins);
+				BinaryTree<T>::insert(ins);
+			}
 		}
 		void delNode(T key, bool discount = false){
 		}
